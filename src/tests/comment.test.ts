26e107be3240f58512bcc908aeb5929d298e2e83
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2022-06-01 21:29:24 -0400

    fix_bugs_v2

--- src/tests/comment.test.ts
+++ src/tests/comment.test.ts
@@ -9,33 +9,33 @@ describe('GET /comment/:page ', ()=>{
     })
 });
 
-describe(`GET /comment/author/:page`, ()=>{
+describe(`POST /comment/author/:page`, ()=>{
     it("List comment by author", async()=>{
-        const result= await request(app).get(`/comment/author/${1}`).send({author:"cercatrova"});
+        const result= await request(app).post(`/comment/author/${1}`).send({author:"cercatrova"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('comment');
     })
 });
 
-describe(`GET /comment/date/:page`, ()=>{
+describe(`POST /comment/date/:page`, ()=>{
     it("List comment by date", async()=>{
-        const result= await request(app).get(`/comment/date/${1}`).send({date:"2022-02-18"});
+        const result= await request(app).post(`/comment/date/${1}`).send({date:"2022-02-18"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('comment');
     })
 });
 
-describe('GET /comment/tag/:page', ()=>{
+describe('POST /comment/tag/:page', ()=>{
     it("List comment by Tag", async()=>{
-        const result= await request(app).get(`/comment/tag/${1}`).send({tag:"story_30361850"});
+        const result= await request(app).post(`/comment/tag/${1}`).send({tag:"story_30361850"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('comment');
     })
 });
 
-describe('GET /comment/filter/page', ()=>{
+describe('POST /comment/filter/page', ()=>{
     it("Filter list comment", async()=>{
-        const result= await request(app).get(`/comment/filter/${1}`).send({author:"ot",tags:["author_ot","comment"]});
+        const result= await request(app).post(`/comment/filter/${1}`).send({author:"ot",tags:["author_ot","comment"]});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('comment');
     })
--- src/tests/storys.test.ts
+++ src/tests/storys.test.ts
@@ -9,41 +9,41 @@ describe('GET /story/:page', ()=>{
     })
 });
 
-describe(`GET /story/author/:page`, ()=>{
+describe(`POST /story/author/:page`, ()=>{
     it("List story by author", async()=>{
-        const result= await request(app).get(`/story/author/${1}`).send({author:"jerrygoyal"});
+        const result= await request(app).post(`/story/author/${1}`).send({author:"jerrygoyal"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('storys');
     })
 });
 
-describe(`GET /story/title/:page`, ()=>{
+describe(`POST /story/title/:page`, ()=>{
     it("List story by title", async()=>{
-        const result= await request(app).get(`/story/title/${1}`).send({title:"Covid"});
+        const result= await request(app).post(`/story/title/${1}`).send({title:"Covid"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('storys');
     })
 });
 
-describe(`GET /story/date/:page`, ()=>{
+describe(`POST /story/date/:page`, ()=>{
     it("List story by date", async()=>{
-        const result= await request(app).get(`/story/date/${1}`).send({date:"2022-02-18"});
+        const result= await request(app).post(`/story/date/${1}`).send({date:"2022-02-18"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('storys');
     })
 });
 
-describe('GET /story/tag/:page', ()=>{
+describe('POST /story/tag/:page', ()=>{
     it("List story by Tag", async()=>{
-        const result= await request(app).get(`/story/tag/${1}`).send({tag:"story"});
+        const result= await request(app).post(`/story/tag/${1}`).send({tag:"story"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('storys');
     })
 });
 
-describe('GET /story/filter/page', ()=>{
+describe('POST /story/filter/page', ()=>{
     it("Filter list story", async()=>{
-        const result= await request(app).get(`/story/filter/${1}`).send({author:"jerrygoyal ", title:"Node"});
+        const result= await request(app).post(`/story/filter/${1}`).send({author:"jerrygoyal ", title:"Node"});
         expect(result.statusCode).toEqual(200);
         expect(result.body).toHaveProperty('storys');
     })
